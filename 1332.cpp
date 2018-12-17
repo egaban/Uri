76cb97cd5ff8b9288d778f6c07192893d937c1ce
Enzo Gaban <egaban@localhost.localdomain>
2018-12-17 17:50:47 -0200

    add 1332

---
+++ 1332.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+int Diferencas(std::string a, std::string b);
+
+int main(void) {
+    int n;
+    std::string entrada;
+
+    std::cin >> n;
+
+    while (n--) {
+        std::cin >> entrada;
+        
+        if (entrada == "one" || Diferencas(entrada, "one") == 1)
+            std::cout << 1;
+        else if (entrada == "two" || Diferencas(entrada, "two") == 1)
+            std::cout << 2;
+        else
+            std::cout << 3;
+        std::cout << "\n";
+    }
+
+    return 0;
+}
+
+int Diferencas(std::string a, std::string b) {
+    int d = 0;
+
+    for (size_t i = 0; i < a.length() && i < b.length(); ++i) {
+        if (a[i] != b[i])
+            d++;
+    }
+
+    d += abs(a.length() - b.length());
+
+    return d;
+}
